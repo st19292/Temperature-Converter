3dd33cba766c2f5a6e936ad0ca87b8e6104fe65c
st19292 <128324391+st19292@users.noreply.github.com>
2023-03-27 15:27:39 +0900

    Update GUI_temp_converter.py
    added help

--- GUI_temp_converter.py
+++ GUI_temp_converter.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from functools import partial   # To prevent unopened windows.
 
 class Converter:
 
@@ -53,8 +54,8 @@ class Converter:
                                        command = lambda: self.temp_convert(-273))
         self.to_farenheit_button.grid(row=0, column=1, padx=5, pady=5)
 
-        self.help_or_info_button = Button(self.button_frame, text="Help/Info",
-                                       bg="#e1d5e7", fg=button_fg, font=button_font, width=12)
+        self.to_help_button = self.help_or_info_button = Button(self.button_frame, text="Help/Info",
+                                       bg="#e1d5e7", fg=button_fg, font=button_font, width=12, command=self.to_help)
         self.help_or_info_button.grid(row=1, column=0, padx=5, pady=5)
 
         self.history_or_export_button = Button(self.button_frame, text="History/Export",
@@ -149,6 +150,52 @@ class Converter:
         
         self.output_label.config(text=output)
             
+    def to_help(self):
+        # Disable the Help/Info button and open the help window
+        self.to_help_button.config(state=DISABLED)
+        DisplayHelp(self)
+
+
+class DisplayHelp:
+    def __init__(self, partner):
+        background = "#f9f4ff"
+
+        self.help_box = Toplevel()
+
+        partner.help_window_open = True
+        self.help_box.protocol('WM_DELETE_WINDOW', partial(self.close_help, partner))
+
+        self.help_frame  = Frame(self.help_box, width=300, height=200, bg=background)
+        self.help_frame.grid()
+        self.help_heading_label = Label(self.help_frame, bg=background, 
+                                        text="Help/Info", 
+                                        font=("Arial", "14", "bold"))
+        self.help_heading_label.grid(row=0)
+        
+        help_text = "To use the program, simply enter the temperature " \
+            "that you wise to convert and then choose convert to either " \
+            "degrees Celcius (centigrade) or Fareheit.. \n\n" \
+            "Note that -273 degrees C (-459F) is absolute zero " \
+            "(the coldest possible temperature). If you convert a " \
+            "temperature that is less than -273C, you will get an error " \
+            "message. \n\n" \
+            "To see your calculation history and export in to a text file, " \
+            "please check the history/export button."
+        
+        self.help_text_label = Label(self.help_frame, bg=background, 
+                                     text=help_text, wrap=350, justify="left")
+        self.help_text_label.grid(row=1, padx=10)
+        
+        self.dismiss_button = Button(self.help_frame, font=("Arial", "12", "bold"), 
+                                     text="Dismiss", bg="#e1d5e7", fg="#232023",
+                                     command=partial(self.close_help, partner))
+        self.dismiss_button.grid(row=2, padx=10, pady=10)
+
+    
+    def close_help(self, partner):
+        partner.to_help_button.config(state=NORMAL)
+        self.help_box.destroy()
+
 
 # Main Program
 if __name__ == "__main__":
